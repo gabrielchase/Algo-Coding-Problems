56469d6e9e8fd1a3b1436d2ca46b1f14daf5717e
gabrielchase <gchase.patron@gmail.com>
2020-01-01 23:15:48 -0700

    Two Sum brute force

---
+++ TwoSum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+    for (let i=0; i < nums.length; i++) {
+        for (let j=i+1; j < nums.length; j++) {
+            console.log(i, j)
+            if (nums[i] + nums[j] === target) {
+              return [ i, j ]
+            }
+        }    
+    }
+};
+
+let input = [3,3]
+console.log(twoSum(input, 6))
