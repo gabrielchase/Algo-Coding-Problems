560d63fa1eb3f6819cd7eb8cdb4c45998584757c
gabrielchase <gchase.patron@gmail.com>
2020-01-05 00:36:16 -0700

    ReverseInteger non recursion, faster solution

--- ReverseInteger.rb
+++ ReverseInteger.rb
@@ -4,48 +4,21 @@
 MAX_LIMIT = (2 ** 31) - 1
 MIN_LIMIT = -(2 ** 31)
 
-
-def get_number_length(x)
-  return x.to_s.length
-end 
-
-def recursive_split(x, arr) 
-  if (get_number_length(x) == 1)
-    arr.push(x)
-    return arr 
-  else  
-    place = (10 ** (get_number_length(x) - 1))
-    val = x / place 
-    arr.push(val)
-    x = x - (val * place)
-
-    # Check for length of x and compare it with place
-    # If difference in length is greater than 1, append another 0
-
-    if ((get_number_length(place) - get_number_length(x)) > 1)
-      count = 1
-      difference = get_number_length(place) - get_number_length(x) 
-      
-      while (count != difference) 
-        arr.push(0)
-        count +=1 
-      end 
-    end 
-
-    return recursive_split(x, arr)
-  end 
-end 
-
 def reverse(x)
   negative = false 
+  reversed = []
   
   if x < 0
     negative = true 
     x = x * -1
   end 
   
-  arr = recursive_split(x, [])
-  val = arr.reverse.join.to_i
+  while x != 0 
+    reversed.push(x % 10)
+    x = x / 10
+  end 
+
+  val = reversed.join.to_i
   
   if negative 
     val = val * -1 
