2e5146916c7a498fe97bd0ec018ed7b892b3a6ff
gabrielchase <gchase.patron@gmail.com>
2020-01-05 00:17:24 -0700

    Reverse Integer finished

--- ReverseInteger.rb
+++ ReverseInteger.rb
@@ -1,23 +1,37 @@
 # @param {Integer} x
 # @return {Integer}
 
+MAX_LIMIT = (2 ** 31) - 1
+MIN_LIMIT = -(2 ** 31)
+
+
 def get_number_length(x)
   return x.to_s.length
 end 
 
 def recursive_split(x, arr) 
-  # puts "currently doing #{x}"
   if (get_number_length(x) == 1)
     arr.push(x)
     return arr 
   else  
     place = (10 ** (get_number_length(x) - 1))
-    # puts "#{place}"
     val = x / place 
-    # puts "pushing #{val}"
     arr.push(val)
     x = x - (val * place)
-    # puts "x: #{x}"
+
+    # Check for length of x and compare it with place
+    # If difference in length is greater than 1, append another 0
+
+    if ((get_number_length(place) - get_number_length(x)) > 1)
+      count = 1
+      difference = get_number_length(place) - get_number_length(x) 
+      
+      while (count != difference) 
+        arr.push(0)
+        count +=1 
+      end 
+    end 
+
     return recursive_split(x, arr)
   end 
 end 
@@ -31,14 +45,15 @@ def reverse(x)
   end 
   
   arr = recursive_split(x, [])
-  
   val = arr.reverse.join.to_i
   
   if negative 
     val = val * -1 
   end 
-  
+
+  if val >= MAX_LIMIT or val <= MIN_LIMIT
+    return 0
+  end
+
   return val 
 end
-
-puts "final arr: ", reverse(901000).to_s
